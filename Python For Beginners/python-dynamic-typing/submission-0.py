9d8f29cdceb9c624987016d889897013097e833c
Tariq <646561+targit86@users.noreply.github.com>
2026-07-18 19:19:30 -0500

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 86
+print(type(variable))
+variable = 8.6
+print(type(variable))
+variable = False
+print(type(variable))
+variable = "hello"
+print(type(variable))
+variable = [8, 6]
+print(type(variable))
