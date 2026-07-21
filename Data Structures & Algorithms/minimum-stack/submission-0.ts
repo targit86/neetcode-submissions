4ccc8d78a5ed42f01fdac652ef679310fa8d3fbb
Tariq <646561+targit86@users.noreply.github.com>
2026-07-21 11:22:24 -0500

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.ts
@@ -0,0 +1,41 @@
+class MinStack {
+    private _stack: number[];
+    constructor() {
+        this._stack = new Array<number>();
+    }
+
+    /**
+     * @param {number} val
+     * @return {void}
+     */
+    push(val: number): void {
+        this._stack.push(val);
+    }
+
+    /**
+     * @return {void}
+     */
+    pop(): void {
+        this._stack.pop();
+    }
+
+    /**
+     * @return {number}
+     */
+    top(): number {
+        return this._stack[this._stack.length - 1];
+    }
+
+    /**
+     * @return {number}
+     */
+    getMin(): number {
+        let min = Infinity;
+        for (let i = 0; i < this._stack.length; i++) {
+            if (this._stack[i] < min) {
+                min = this._stack[i];
+            }
+        }
+        return min;
+    }
+}
