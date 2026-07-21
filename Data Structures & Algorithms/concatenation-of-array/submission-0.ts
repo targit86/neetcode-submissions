3721d6ac5b7883ffd8b2f21dbff24a2942f04dad
Tariq <646561+targit86@users.noreply.github.com>
2026-07-20 21:30:25 -0500

    Add: concatenation-of-array - submission-0

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-0.ts
@@ -0,0 +1,12 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    getConcatenation(nums: number[]): number[] {
+        return [
+            ...nums,
+            ...nums
+        ]
+    }
+}
